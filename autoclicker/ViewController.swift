d3a88cb37e6e716ac67f0f06c499a86d13ea52c3
Jach Yeh <jack59074@gmail.com>
2019-12-28 21:11:47 +0800

    add click func

--- autoclicker/ViewController.swift
+++ autoclicker/ViewController.swift
@@ -9,19 +9,79 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    override var acceptsFirstResponder: Bool { return true }
+    override func becomeFirstResponder() -> Bool { return true }
+    override func resignFirstResponder() -> Bool { return true }
+    
+    lazy var window: NSWindow = self.view.window!
+    var mouseLocation: NSPoint { NSEvent.mouseLocation }
+    var location: NSPoint { window.mouseLocationOutsideOfEventStream }
+    var remained: Int = 0
+    
+    
+    @IBOutlet weak var speedNum: NSTextField!
+    @IBOutlet weak var clickCount: NSTextField!
+    @IBOutlet weak var button: NSButton!
+    @IBOutlet weak var remainedCount: NSTextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        NSEvent.addGlobalMonitorForEvents(matching: .keyDown, handler: self.keyDown)
     }
 
+    override func keyDown(with event: NSEvent) {
+        print(event.keyCode)
+        if event.keyCode == 96 {
+             switchStatus()
+         }
+    }
+    
+    @IBAction func clickButton(_ sender: Any) {
+        switchStatus()
+    }
+    
+    func switchStatus() {
+        if self.remained > 0  {
+            self.setRemainedCount(c:0)
+        } else {
+            let count = NSDecimalNumber.init(string: self.clickCount.stringValue).intValue
+            self.setRemainedCount(c: count)
+            simulateMouseClick()
+        }
+    }
+    
+    func setRemainedCount(c: Int)  {
+        self.remained = c
+        self.remainedCount.stringValue = String(c)
+    }
+    
+    func simulateMouseClick() {
+        if self.remained <= 0 {
+            return
+        }
+            
+        self.remained -= 1
+        
+        let point = CGPoint(x:self.mouseLocation.x, y:self.mouseLocation.y)
+        let mouseDown = CGEvent(mouseEventSource: nil, mouseType: .leftMouseDown, mouseCursorPosition: point, mouseButton: .left)
+        let mouseUp = CGEvent(mouseEventSource: nil, mouseType: .leftMouseUp, mouseCursorPosition: point, mouseButton: .left)
+        print (point)
+        mouseDown?.post(tap: .cghidEventTap)
+        mouseUp?.post(tap: .cghidEventTap)
+        
+        let sec = NSDecimalNumber.init(string: self.speedNum.stringValue).intValue
+        usleep(useconds_t(sec))
+        
+        simulateMouseClick()
+    }
+    
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
     }
-
-
 }
 
