0dc9bb9f27f19399639402709db6277398fb9b45
Jach Yeh <jack59074@gmail.com>
2019-12-28 20:15:08 +0800

    Initial Commit

---
+++ autoclicker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  autoclicker
+//
+//  Created by Apple on 2019/12/28.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ autoclicker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  autoclicker
+//
+//  Created by Apple on 2019/12/28.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
